779c378af589f729246ff83904e19374801c56d2
Rakiba Chowdhury <chow0165@algonquinlive.com>
2026-01-28 00:07:50 -0500

    Initial project setup with record class, dataset, and main program

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+CST8002 - Programming Language Research Project
+Practical Project Part 1 - Proof of Concept
+Professor: Stanley Pieda
+Due Date: February 1, 2026
+Student: Rakiba Chowdhury
+Section: 020
+
+Dataset Source:
+Parks Canada. (2017). Black Oystercatcher Population – Pacific Rim.
+Available at: https://open.canada.ca/data/en/dataset/d87383f6-5313-430d-8416-1b6d6e377e02
+License: Open Government Licence – Canada
+
+This program loads data from a CSV file and displays 
+it using custom Record objects stored in alost.
+"""
+
+def main():
+    print("Hello from my cst8002 Practical Project!")
+
+    if __name__ == "__main__":
+        main()
+
