8c3255f8b795921a79466c018f45fb49b671a1ae
Rakiba Chowdhury <chow0165@algonquinlive.com>
2026-03-23 17:28:37 -0400

    Update main.py header to Part 3, Part 3 complete

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 """
 CST8002 - Programming Language Research Project
-Practical Project Part 2 - Project Review I
+Practical Project Part 3 - Project Review 2
 Professor: Stanley Pieda
-Due Date: February 22, 2026
+Due Date: March 29, 2026
 Student: Rakiba Chowdhury
 Section: 020
 
@@ -16,22 +16,24 @@ Available at: https://open.canada.ca/data/en/dataset/d87383f6-5313-430d-8416-1b6
 License: Open Government Licence - Canada
 
 References:
-[1] Python Software Foundation, "Modules," Python 3.12 Documentation.
-    [Online]. Available: https://docs.python.org/3/tutorial/modules.html
-    [Accessed Feb. 15, 2026]
+[1] Python Software Foundation. (2024). Modules. docs.python.org [Online]. 
+Available: https://docs.python.org/3/tutorial/modules.html [Accessed: Feb. 15, 2026].
+[2] Python Software Foundation. (2024). Sorting HOW TO. docs.python.org [Online].
+Available: https://docs.python.org/3/howto/sorting.html [Accessed: Mar. 20, 2026].
 """
 from presentation.menu import Menu
+
 
 def main():
     """
     Main function that starts the program.
 
-    Creates a Menu instance (presentation layer) and runs the interactive 
+    Creates a Menu instance (presentation layer) and runs the interactive
     menu loop for the user.
     """
     app = Menu()
     app.run()
 
+
 if __name__ == "__main__":
     main()
-
